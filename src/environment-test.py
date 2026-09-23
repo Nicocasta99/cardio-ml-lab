6df9d7af970037584d3ad3ffaf3b0c594f871eb3
nicolas <nicolascastacardona@gmail.com>
2026-09-23 04:14:08 +0000

    Environment test aproved

---
+++ src/environment-test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model as linear_model
+
+print('Numpy: ',np.__version__)
+print('Pandas: ',pd.__version__)
+
+print('Environment ready!')
